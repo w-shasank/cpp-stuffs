98dc81ad27816d5f9e5148202ae386a9e911ab78
Shasank-w <shasankwagle0@gmail.com>
2025-07-25 21:53:08 +0545

    Grasping the concept of virtual destructors

---
+++ oop_questions/virtual_destructors.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+class base{
+    protected:
+        int baseVal;
+    public:
+     base(int x=0):baseVal(x)
+    {
+        cout<<"Base Constructor called";
+    }
+    ~base()
+    {
+        cout<<endl<<"Base destructor called";
+    }
+};
+class derived : public base{
+    private:
+    int derivedValue;
+public:
+   derived(int x,int y):base(x),derivedValue(y){
+    cout<<"Derived Constructor Called :";
+   }
+   ~derived()
+   {
+    cout<<endl<<"Derived destructor called :";
+   }
+};
+
+
+int main()
+{
+    base *b = new derived(10,20);
+    return 0;
+}
