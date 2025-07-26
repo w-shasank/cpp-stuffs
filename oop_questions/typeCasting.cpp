3d314aeb5659ff8ec1b5167310ac3529e3a26303
w-shasank <shasankwagle0@gmail.com>
2025-07-26 22:05:09 +0545

    Reinterpret type casting

---
+++ oop_questions/typeCasting.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+class Base {
+   protected :
+        int baseNumber;
+    public:
+    Base(int x=0):baseNumber(x)
+    {}
+    virtual ~Base()
+    {
+        cout<<endl<<"The destructor is called :";
+    }
+};
+class Derived :public Base{
+    private :
+        int derivedNumber ;
+    public:
+        Derived(int First=0,int Second =0):Base(First),derivedNumber(Second)
+        {
+            cout<<endl<<"The constructor of the base class is called :";
+
+        }
+};
+int main()
+{
+    int num=56;
+    char *b = reinterpret_cast<char*>(num);
+    cout<<endl<<static_cast<void *>(b);
+    return 0;
+}
