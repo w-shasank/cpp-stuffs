e39a5db9b29c19ed143d534e49bd83429bae48e3
w-shasank <shasankwagle0@gmail.com>
2025-07-29 06:32:53 +0545

    POSIX API : fork()

---
+++ ostest/forkp1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int main(int argc,char *argv[])
+{
+    printf("Hello world !! (pid : %d)\n",(int)getpid());
+    int rc =fork();
+    if(rc<0)
+    {
+        fprintf(stderr,"Fork Failed :\n");
+        exit(1);
+    }
+    else if (rc==0)
+    {
+        printf("Hello I am child (pid ; %d)",(int)getpid()); 
+    }
+    else{
+        printf("Hello, I am a parent of %d (pid : %d)\n",(int)getpid(),rc );
+    }
+
+    return 0;
+}
